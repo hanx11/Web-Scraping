6cd40e57d478e1eadba9284349a0b1348e41a3f7
hanx11 <hanx11@163.com>
2016-01-20 10:30:04 +0800

    modify douban-movies-crawler.py

--- douban-movies-crawler.py
+++ douban-movies-crawler.py
@@ -1,47 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import requests
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 
-def get_tag_link_list():
-	#获取豆瓣电影分类标签页中的各分类链接url
-	tag_link_list = set()
+def get_tag_table():
+	# 获取豆瓣电影分类标签中的分类列表
 	try:
-		response = urlopen('http://movie.douban.com/tag/')
-	except HTTPError as e:
+		response = requests.get('https://movie.douban.com/tag/')
+	except Exception as e:
 		raise e
 	else:
-		html = response.read()
-		bsObj = BeautifulSoup(html, 'html.parser')
-		tableList = bsObj.findAll('table', {'class':'tagCol'})
+		bsObj = BeautifulSoup(response.content, 'html.parser')
+		tableList =  bsObj.findAll('table', {'class':'tagCol'})
+		return tableList
 
+
+def get_tag_link_list(tableList):
+	# 获取豆瓣电影分类标签页中的各分类列表中的链接url
+	tag_link_list = set()
 	for table in tableList:
 		tmplist = table.tbody.findAll('a')
 		for link in tmplist:
 			tag_link_list.add(link.get('href'))
 	return tag_link_list
 
-link_list = get_tag_link_list()
-for link in link_list:
-	try:
-		response = urlopen(link.encode('utf-8'))
-	except HTTPError as e:
-		raise e
-	else:
-		html = response.read()
-		bsObj = BeautifulSoup(html, 'html.parser')
-		print(bsObj.prettify())
+# link_list = get_tag_link_list()
+# for link in link_list:
+# 	try:
+# 		response = urlopen(link.encode('utf-8'))
+# 	except HTTPError as e:
+# 		raise e
+# 	else:
+# 		html = response.read()
+# 		bsObj = BeautifulSoup(html, 'html.parser')
+# 		print(bsObj.prettify())
 
 
-'''
 def main():
-	link_list = get_tag_link_list()
-	for link in link_list:
+	tableList = get_tag_table()
+	linkList = get_tag_link_list(tableList)
+	for link in linkList:
 		print(link)
 
 if __name__ == '__main__':
 	main()
-'''
