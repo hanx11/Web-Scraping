d36fe2e9830837994bcc91785dc88c5cce322392
hanx11 <hanx11@163.com>
2016-01-25 15:04:41 +0800

    modify douban-movies-crawler.py

--- douban-movies-crawler.py
+++ douban-movies-crawler.py
@@ -6,7 +6,42 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
+topic = [
+			{'topic_id':'60443', 'topic_name':'爱情'},
+			{'topic_id':'62355', 'topic_name':'喜剧'},
+			{'topic_id':'62356', 'topic_name':'动画'},
+			{'topic_id':'62359', 'topic_name':'剧情'},
+			{'topic_id':'60454', 'topic_name':'科幻'},
+			{'topic_id':'62360', 'topic_name':'动作'},
+			{'topic_id':'62358', 'topic_name':'经典'},
+			{'topic_id':'60979', 'topic_name':'悬疑'},
+			{'topic_id':'61527', 'topic_name':'青春'},
+			{'topic_id':'62364', 'topic_name':'犯罪'},
+			{'topic_id':'62363', 'topic_name':'惊悚'},
+			{'topic_id':'60434', 'topic_name':'文艺'},
+			{'topic_id':'60334', 'topic_name':'纪录片'},
+			{'topic_id':'209', 'topic_name':'搞笑'},
+			{'topic_id':'60521', 'topic_name':'励志'},
+			{'topic_id':'62369', 'topic_name':'恐怖'},
+			{'topic_id':'62371', 'topic_name':'战争'},
+			{'topic_id':'62370', 'topic_name':'短片'},
+			{'topic_id':'62372', 'topic_name':'魔幻'},
+			{'topic_id':'61810', 'topic_name':'黑色幽默'},
+			{'topic_id':'62376', 'topic_name':'传记'},
+			{'topic_id':'62375', 'topic_name':'情色'},
+			{'topic_id':'62374', 'topic_name':'动画短片'},
+			{'topic_id':'62377', 'topic_name':'感人'},
+			{'topic_id':'62378', 'topic_name':'暴力'},
+			{'topic_id':'62383', 'topic_name':'浪漫'},
+			{'topic_id':'61554', 'topic_name':'女性'},
+			{'topic_id':'62381', 'topic_name':'同志'},
+			{'topic_id':'62386', 'topic_name':'史诗'},
+			{'topic_id':'62387', 'topic_name':'童话'},
+			{'topic_id':'62388', 'topic_name':'烂片'},
+			{'topic_id':'62389', 'topic_name':'cult'}
+		]
 
+		
 def get_tags():
 	# 获取豆瓣电影分类标签
 	tagList = []
